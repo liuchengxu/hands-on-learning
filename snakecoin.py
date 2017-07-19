82ad3f44dfaed2f22c4753757fdc6884840aeb81
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2017-07-19 20:08:17 +0800

    Add snakecoin.py

---
+++ snakecoin.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import hashlib as hasher
+import datetime as date
+
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.hash_block()
+
+    def hash_block(self):
+        sha = hasher.sha256()
+        sha.update(
+            bytes(
+                str(self.index) + str(self.timestamp) + str(self.data) + str(
+                    self.previous_hash), 'utf-8'))
+        return sha.hexdigest()
+
+
+def create_genesis_block():
+    #  Manually construct a block with index 0 and arbitrary previous hash
+    return Block(0, date.datetime.now(), "Genesis Block", "0")
+
+
+def next_block(last_block):
+    this_index = last_block.index + 1
+    this_timestamp = date.datetime.now()
+    this_data = "Hey! I'm block " + str(this_index)
+    this_hash = last_block.hash
+    return Block(this_index, this_timestamp, this_data, this_hash)
+
+
+def main():
+    #  Create the blockchain and add the genesis block
+    blockchain = [create_genesis_block()]
+    previous_block = blockchain[0]
+
+    #  How many blocks should we add to the chain after the genesis block
+    num_of_blocks_to_add = 20
+
+    for i in range(0, num_of_blocks_to_add):
+        block_to_add = next_block(previous_block)
+        blockchain.append(block_to_add)
+        previous_block = block_to_add
+        #  Tell everyone about it!
+        print("Block #{} has been added to the"
+              "blockchain!".format(block_to_add.index))
+        print("Hash: {}\n".format(block_to_add.hash))
+
+
+if __name__ == "__main__":
+    main()
